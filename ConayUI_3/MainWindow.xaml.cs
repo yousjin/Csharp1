4437bdf76b048bf63273d26c429eef3a54fde237
yousjin <67782474+yousjin@users.noreply.github.com>
2022-05-03 21:15:20 +0900

    Add files via upload

---
+++ ConayUI_3/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConayUI_3
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HeartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DiaryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DiaryButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HeartButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
